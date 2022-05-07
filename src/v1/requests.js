f340d206431eb8df7cfdff8fd84e6d40042a6e09
Jeweetzelf <67839219+jeweetzelfWasTaken@users.noreply.github.com>
2022-05-07 15:19:24 +0200

    ig added ratelimit function idk if it works tho

---
+++ src/v1/requests.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+const Client = require('./Client');
+
+/**
+ * Make an axios request
+ * @param {string} path The path to the endpoint
+ * @param {string} APIKey The APIKey of the Client
+ * @param {'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'} method The method to use
+ * @param {object} body The body of the request
+ * @param {Client} client The PteroManager Client
+ * @returns {Promise<object>} The response of the request
+ */
+module.exports = async function (path, APIKey, method, body, client) {
+    console.log(client._requestsRemaining)
+    await checkRateLimit(client)
+    return new Promise((resolve, reject) => {
+        axios.default(path, {
+            headers: {
+                'Authorization': `Bearer ${APIKey}`,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: method,
+            data: body
+        }).then(res => {
+            client._requestsRemaining = res.headers['x-ratelimit-remaining'];
+            resolve(res.data);
+        }).catch(err => {
+            client._requestsRemaining = err.response.headers['x-ratelimit-remaining'];
+            reject(err);
+        })
+    })
+}
+
+async function checkRateLimit(client) {
+    if (client._requestsRemaining <= 0 /* change this to 0 later */) {
+        let toReturn = await new Promise((resolve, reject) => {
+            let timeToWait = (60 - new Date().getSeconds()) * 1000;
+            console.log(timeToWait)
+            setTimeout(async () => {
+                if (client._requestsRemaining <= 0 /* change this to 0 later */) {
+                    return resolve(await checkRateLimit(client));
+                }
+                resolve(true);
+            }, timeToWait)
+        })
+
+        return toReturn;
+    }
+    else return true;
+}
